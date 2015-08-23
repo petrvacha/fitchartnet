1d19e3462c1906c842221a073a170f0fefdd196d
Petr Vacha <pan.vacha@gmail.com>
2015-08-23 19:42:57 +0200

    list of records

--- app/model/ActivityLog.php
+++ app/model/ActivityLog.php
@@ -8,5 +8,13 @@ namespace App\Model;
  */
 class ActivityLog extends BaseModel
 {
-
+    /**
+     * @param int $userId
+     * @param int $activityId
+     * @return mixed
+     */
+    public function getUserActities($userId, $activityId)
+    {
+        return $this->findBy(['user_id' => $userId, 'activity_id' => $activityId, 'active' => TRUE])->fetchAll();
+    }
 }
--- app/presenters/MainPresenter.php
+++ app/presenters/MainPresenter.php
@@ -8,13 +8,33 @@ namespace App\Presenters;
  */
 class MainPresenter extends LoginBasePresenter
 {
+    /** @var \App\Model\ActivityLog */
+    protected $activityLog;
+
+    
     /** @var \IActivityFormFactory @inject */
     public $activityFormFactory;
+
     
+    /**
+     * @param \App\Model\ActivityLog $activityLog
+     */
+    public function __construct(\App\Model\ActivityLog $activityLog)
+    {
+        $this->activityLog = $activityLog;
+    }
 
     public function renderDefault()
     {
         
+    }
+
+    /**
+     * @param int $id activity_id
+     */
+    public function actionList($id)
+    {
+        $this->template->activities = $this->activityLog->getUserActities($this->user->id, $id);
     }
 
     /**
