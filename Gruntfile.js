272f8423d11e7f35e7722635c2c305b38365dfa8
Petr Vacha <pan.vacha@gmail.com>
2020-03-08 19:54:09 +0100

    bootstrap4

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,36 @@
 module.exports = function(grunt) {
     require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
+    var newCssFiles = [
+        './node_modules/startbootstrap-sb-admin/vendor/datatables/dataTables.bootstrap4.css',
+        './node_modules/startbootstrap-sb-admin/css/sb-admin.css',
+        './node_modules/startbootstrap-sb-admin/vendor/fontawesome-free/css/all.min.css',
+        './htdocs/css/libs/token-input-facebook.css',
+        './htdocs/css/libs/bootstrap.css',
+        './htdocs/css/style-new.css'
+    ];
+
+    var newJsFiles = [
+        './htdocs/js/libs/morris.min.js',
+        './htdocs/js/libs/jquery.flot.js',
+        './htdocs/js/libs/jquery.flot.time.js',
+        './htdocs/js/libs/jquery.flot.pie.min.js',
+        './htdocs/js/libs/jquery.flot.tooltip.js',
+        './htdocs/js/libs/jquery.tokeninput.js',
+        './node_modules/startbootstrap-sb-admin/vendor/bootstrap/js/bootstrap.bundle.min.js',
+        './node_modules/startbootstrap-sb-admin/vendor/jquery-easing/jquery.easing.min.js',
+        './node_modules/startbootstrap-sb-admin/vendor/chart.js/Chart.min.js',
+        './node_modules/startbootstrap-sb-admin/vendor/datatables/jquery.dataTables.js',
+        './htdocs/js/libs/raphael-min.js',
+        './htdocs/js/libs/netteForms.js',
+        './htdocs/js/libs/jquery.datetimepicker.js',
+        './htdocs/js/libs/nette.ajax.js',
+        './htdocs/js/libs/cropper.min.js',
+        './node_modules/startbootstrap-sb-admin/vendor/datatables/dataTables.bootstrap4.js',
+        './node_modules/startbootstrap-sb-admin/js/sb-admin.js',
+        './htdocs/js/common.js'
+    ];
+
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         cssmin: {
@@ -17,17 +48,15 @@ module.exports = function(grunt) {
                         './htdocs/css/style.css'
                     ],
 
-                    './htdocs/css/style-new.min.css': [
-                        './node_modules/startbootstrap-sb-admin/vendor/datatables/dataTables.bootstrap4.css',
-                        './node_modules/startbootstrap-sb-admin/css/sb-admin.css',
-                        './node_modules/startbootstrap-sb-admin/vendor/fontawesome-free/css/all.min.css',
-                        './htdocs/css/style-new.css'
-                    ]
+                    './htdocs/css/style-new.min.css': newCssFiles
                 }
             }
         },
 
         uglify: {
+            options: {
+                sourceMap: true
+            },
             web: {
                 files: {
                     './htdocs/js/script.min.js': [
@@ -47,29 +76,7 @@ module.exports = function(grunt) {
                         './htdocs/js/common.js'
                     ],
 
-                    './htdocs/js/script-new.min.js': [
-                        './node_modules/startbootstrap-sb-admin/vendor/jquery/jquery.min.js',
-                        './node_modules/startbootstrap-sb-admin/vendor/jquery-easing/jquery.easing.min.js',
-                        './htdocs/js/libs/jquery.flot.js',
-                        './htdocs/js/libs/jquery.flot.time.js',
-                        './htdocs/js/libs/jquery.flot.pie.min.js',
-                        './htdocs/js/libs/jquery.flot.tooltip.js',
-                        './htdocs/js/libs/jquery.tokeninput.js',
-                        './node_modules/startbootstrap-sb-admin/vendor/bootstrap/js/bootstrap.bundle.min.js',
-                        './node_modules/startbootstrap-sb-admin/vendor/jquery-easing/jquery.easing.min.js',
-                        './node_modules/startbootstrap-sb-admin/vendor/chart.js/Chart.min.js',
-                        './node_modules/startbootstrap-sb-admin/vendor/datatables/jquery.dataTables.js',
-                        './htdocs/js/libs/raphael-min.js',
-                        './htdocs/js/libs/morris.min.js',
-                        './htdocs/js/libs/netteForms.js',
-                        './htdocs/js/libs/jquery.datetimepicker.js',
-                        './htdocs/js/libs/nette.ajax.js',
-                        './htdocs/js/libs/cropper.min.js',
-                        './node_modules/startbootstrap-sb-admin/vendor/datatables/jqueryTables.js',
-                        './node_modules/startbootstrap-sb-admin/vendor/datatables/dataTables.bootstrap4.js',
-                        './node_modules/startbootstrap-sb-admin/js/sb-admin.min.js',
-                        './htdocs/js/common.js'
-                    ]
+                    './htdocs/js/script-new.min.js': newJsFiles
                 }
             }
         },
@@ -94,27 +101,7 @@ module.exports = function(grunt) {
                         './htdocs/js/common.js'
                     ],
 
-                    './htdocs/js/script-new.min.js': [
-                        './node_modules/startbootstrap-sb-admin/vendor/jquery/jquery.min.js',
-                        './htdocs/js/libs/jquery.flot.js',
-                        './htdocs/js/libs/jquery.flot.time.js',
-                        './htdocs/js/libs/jquery.flot.pie.min.js',
-                        './htdocs/js/libs/jquery.flot.tooltip.js',
-                        './htdocs/js/libs/jquery.tokeninput.js',
-                        './node_modules/startbootstrap-sb-admin/vendor/bootstrap/js/bootstrap.bundle.min.js',
-                        './node_modules/startbootstrap-sb-admin/vendor/jquery-easing/jquery.easing.min.js',
-                        './node_modules/startbootstrap-sb-admin/vendor/chart.js/Chart.min.js',
-                        './node_modules/startbootstrap-sb-admin/vendor/datatables/jquery.dataTables.js',
-                        './htdocs/js/libs/raphael-min.js',
-                        './htdocs/js/libs/morris.min.js',
-                        './htdocs/js/libs/netteForms.js',
-                        './htdocs/js/libs/jquery.datetimepicker.js',
-                        './htdocs/js/libs/nette.ajax.js',
-                        './htdocs/js/libs/cropper.min.js',
-                        './node_modules/startbootstrap-sb-admin/vendor/datatables/dataTables.bootstrap4.js',
-                        './node_modules/startbootstrap-sb-admin/js/sb-admin.js',
-                        './htdocs/js/common.js'
-                    ]
+                    './htdocs/js/script-new.min.js': newJsFiles
                 }
             }
         },
